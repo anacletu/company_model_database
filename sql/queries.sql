4c63c9ebf1aa5143769ce0eb11c75b4c5e5dbca8
Eric <eric.anacleto@gmail.com>
2024-04-20 10:44:52 +0200

    Code for graph generation on Apex

--- sql/queries.sql
+++ sql/queries.sql
@@ -4,6 +4,37 @@
 --   2024-04-18; last update on 2024-04-19
 -- ------------------------------------------------------------------------------
 
--- Change the date format to DD/MM/YYYY to avoid the need to use TO_CHAR in the queries
-ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY';
 
+-- ------------------------------------------------------------------------------
+-- Code used for generating the charts (Distribution Analysis)
+-- ------------------------------------------------------------------------------
+--- Revenue From Last Orders
+SELECT
+    OrderNumber,
+    TotalPrice
+FROM
+    (
+        SELECT
+            OrderNumber,
+            TotalPrice,
+            ROW_NUMBER() OVER (ORDER BY OrderNumber DESC) AS row_num
+        FROM
+            SalesOrders
+    )
+WHERE
+    row_num <= 5
+ORDER BY
+    OrderNumber;
+
+-- Employees per Department
+SELECT DepartmentName AS Department, COUNT(*) AS Employees
+FROM Employees
+JOIN Departments ON Employees.DepartmentNumber = Departments.DepartmentNumber
+GROUP BY DepartmentName
+ORDER BY DepartmentName;
+
+-- Highest Salaries
+SELECT FirstName || ' ' || LastName || ' - ' || JobTitle AS Employee, Salary AS Salary
+FROM Employees
+ORDER BY Salary DESC
+FETCH FIRST 5 ROWS ONLY;
